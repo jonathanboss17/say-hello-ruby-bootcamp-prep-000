7931db22af3658ccb5af7da0240ddc4911512dd5
dynamic-router-3131 <jtboss47@gmail.com>
2019-07-08 21:48:45 +0000

    Automatically backed up by Learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,6 +1,6 @@
 # Build your say_hello method here
 
-def say_hello(name = "Ruby Programmer")
+def say_hello(name = "Ruby Programmer") #default argument (can add more than one)
   puts "Hello " + name + "!"
 end
 
